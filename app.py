e15946ccc1d4c5ad7f6a57c38f17bd6e769fab78
Chen <xuelian.chen@accenture.com>
2020-08-10 00:19:41 +0800

    add default route

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ url  = "http://www.baidu.com"
 res = requests.get(url)
 content = res.text
 app = Flask(__name__)
-@app.route('/hello')
+@app.route('/baidu')
 def index():
     # return 'Hello World'
     return content
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
 if __name__ == '__main__':
     app.debug = True # 设置调试模式，生产模式的时候要关掉debug
     app.run()
